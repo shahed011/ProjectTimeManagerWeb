7ea981407bcad23383cf070c7956db3617e7d172
Shahed <Muhammad.Aziz@KPMG.co.uk>
2016-09-06 00:23:48 +0100

    Initial project structure.

---
+++ ProjectTimeManager/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    var $sidebarAndWrapper = $("#sidebar","#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $("#sidebar").toggleClass("hide-sidebar");
+        $("#wrapper").toggleClass("hide-sidebar");
+    });
+
+    //var ele = $("#username");
+    //ele.text("Shahed");
+
+    //var main = $("#main");
+    //$("#main").on("mouseenter", function () {
+    //    $("#main").style.backgroundColor = "#888";
+    //});
+
+    //$("#main").on("mouseleave", function () {
+    //    $("#main").style.backgroundColor = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    alert($(this).text());
+    //});
+})();
